d763d5a90b14c635eec1735dffc4aaf86849b4bc
alexsedofia <mycelin360@gmail.com>
2023-02-17 11:15:35 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar((num % 9) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
